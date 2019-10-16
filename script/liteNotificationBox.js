c184df9d8cd0dce9716a2bf2bde3306a194d5f7d
Fırat ÖZ <firatoz01@gmail.com>
2019-10-16 17:58:17 +0300

    Update liteNotificationBox.js

--- script/liteNotificationBox.js
+++ script/liteNotificationBox.js
@@ -81,7 +81,7 @@ window.liteNotificationBox = {
             this.constAnimation[preAnimation][0] + ";}" +
             ".ltNotificationBox.active{" + this.constAnimation[preAnimation][1] + ";}" +
             ".ltImg{width:" + this.width + "px; height: " + this.height + "px;}" +
-            ".ltCloseEl{position: absolute;display:inline-block; top: 2%;right: 4%;cursor: pointer; color:#fff;border-radius: 50%;border: 2px solid;width: 10%;height:10%;text-align: center;}" +
+            ".ltCloseEl{position: absolute;display:inline-block; top: 2%;right: 4%;cursor: pointer; color:#fff;border-radius: 50%;border: 2px solid;width: 8%;height:8%;text-align: center;}" +
             this.css
         );
 
